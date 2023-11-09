243ad12eb299938d1e0984e06a5127e33f4c59c9
Dylan Butler <dylanbutler@outlook.com.au>
2023-11-09 21:18:12 +1100

    test list all

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/dbut2/technical-writer
 
 go 1.21.4
+
+require github.com/sashabaranov/go-openai v1.17.4 // indirect
--- main.go
+++ main.go
@@ -3,24 +3,66 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/sashabaranov/go-openai"
 )
 
 func main() {
-	listAllFiles()
+	args := os.Args
+	if len(args) != 1 {
+		panic("incorrect args passed, should be 1")
+	}
+
+	openaiToken := args[0]
+
+	client := openai.NewClient(openaiToken)
+	_ = client
+
+	files, err := listAllFiles("/github/workspace")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for _, file := range files {
+		err = document(file)
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+
+	fmt.Println(files)
 }
 
-func listAllFiles() {
-	file, err := os.Open("/github/workspace")
+func listAllFiles(dir string) ([]string, error) {
+	var files []string
+
+	file, err := os.Open(dir)
 	if err != nil {
-		panic(err.Error())
+		return nil, err
+	}
+	defer file.Close()
+
+	fileInfos, err := file.ReadDir(0)
+	if err != nil {
+		return nil, err
 	}
 
-	files, err := file.Readdir(0)
-	if err != nil {
-		panic(err.Error())
+	for _, fileInfo := range fileInfos {
+		if fileInfo.IsDir() {
+			subFiles, err := listAllFiles(fileInfo.Name())
+			if err != nil {
+				return nil, err
+			}
+			files = append(files, subFiles...)
+		} else {
+			files = append(files, dir+"/"+fileInfo.Name())
+		}
 	}
 
-	for _, f := range files {
-		fmt.Println(f.Name())
-	}
+	return files, nil
+}
+
+func document(file string) error {
+	fmt.Println(file)
+	return nil
 }
