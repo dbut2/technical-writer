8270238d6044e37ad1b8d74f235f52ecf76de885
Dylan Butler <dylanbutler@outlook.com.au>
2023-11-09 21:04:05 +1100

    list github files

--- main.go
+++ main.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("This runs!")
+	listAllFiles()
+}
+
+func listAllFiles() {
+	file, err := os.Open("/github/workspace")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	files, err := file.Readdir(0)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for _, f := range files {
+		fmt.Println(f.Name())
+	}
 }
